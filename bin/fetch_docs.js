2ce6a3bbf70426df4cd7c684498c9d00680799c8
Sebastien Douheret <sebastien.douheret@iot.bzh>
2018-12-20 00:44:11 +0100

    Fixed images auto-retrieval from markdown content
    Change-Id: Ie0bfd0c249dd6d688032f6b76f170e06aaffe5a1
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- bin/fetch_docs.js
+++ bin/fetch_docs.js
@@ -28,19 +28,27 @@ var VERBOSE;
 
 //parse markdown to get images
 async function parseMarkdown(contents, url, pathMd) {
-    var regexSection = /(\(|src=")(.+\.(jpg|png|pdf|svg))(\)|")/ig;
-    //src="images/app-developer-workflow.png"
 
-    var imageRes;
-    while ((imageRes = regexSection.exec(contents)) !== null) {
-        var image = imageRes[2].replace(/\s+/, "");
-        //ignore links
-        if (!image.startsWith("http")) {
-            var imageUrl = url.replace(path.basename(pathMd), image);
-            var imageDst = path.join(path.dirname(pathMd), image);
-            downloadFile(imageUrl, imageDst, false);
+    var regexArr = [
+        // Match reference   ![make-units](pictures/make-units.svg)
+        /\[.*\]\((.*\.(jpg|png|pdf|svg))\)/ig,
+
+        // Match reference   [afm-daemons]: pictures/afm-daemons.svg
+        /\[.*\]: *(.*\.(jpg|png|pdf|svg))/ig,
+    ];
+
+    regexArr.forEach(function(rx) {
+        var imageRes;
+        while ((imageRes = rx.exec(contents)) !== null) {
+            var image = imageRes[1].replace(/\s+/, "");
+            //ignore links
+            if (!image.startsWith("http")) {
+                var imageUrl = url.replace(path.basename(pathMd), image);
+                var imageDst = path.join(path.dirname(pathMd), image);
+                downloadFile(imageUrl, imageDst, false);
+            }
         }
-    }
+    });
 }
 
 /*needed to be proceed by jekyll*/
